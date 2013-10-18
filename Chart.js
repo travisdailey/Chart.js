1bb46042276331ecf177902f70d0ad21b343b1c3
Josh Pigford <josh@joshpigford.com>
2013-10-18 09:04:18 -0500

    MicroTemplating with Curly Braces
    Uses John Resig's MicroTemplating with curly braces so this can be used in Rails ERB templates.

--- Chart.js
+++ Chart.js
@@ -1731,12 +1731,12 @@ window.Chart = function(context, options){
 			// Convert the template into pure JavaScript
 			str
 			  .replace(/[\r\t\n]/g, " ")
-			  .split("<%").join("\t")
-			  .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-			  .replace(/\t=(.*?)%>/g, "',$1,'")
-			  .split("\t").join("');")
-			  .split("%>").join("p.push('")
-			  .split("\r").join("\\'")
+           		  .split("{%").join("\t")
+           		  .replace(/((^|%})[^\t]*)'/g, "$1\r")
+           		  .replace(/\t=(.*?)%}/g, "',$1,'")
+           		  .split("\t").join("');")
+           		  .split("%}").join("p.push('")
+           		  .split("\r").join("\\'")
 		  + "');}return p.join('');");
 	   
 		// Provide some basic currying to the user
